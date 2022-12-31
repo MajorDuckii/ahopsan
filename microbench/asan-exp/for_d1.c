48f0916231699c8ffec43ad63f5354f0dd85f173
YUAN Pinghai <pinghaiyuan@gmail.com>
2022-12-31 14:07:03 +0800

    add for*.c

---
+++ microbench/asan-exp/for_d1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int a[8] = {0};
+  int b[8] = {0};
+  b[1] = 1;
+int i = 0, x = 0, y = 0;
+  for (; i < 8; ++i) // uh oh
+  {
+    // This loop looks like it only reads / writes inside b
+    b[i] += x + y;
+    y = x;
+    x = b[i];
+  }
+
+	i = 7, x = 0, y = 0;
+  for (; i >= 0; --i) // uh oh
+  {
+    // This loop looks like it only reads / writes inside b
+    b[i] += x + y;
+    y = x;
+    x = b[i];
+  }
+
+  // a has been altered, b[8] is actually a[0]
+  return a[0];
+}
